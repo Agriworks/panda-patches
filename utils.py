9a9ea23eb8e60b1efa09164f835a5c7a3ced8192
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-11-04 04:10:59 +0530

    Feature/other value handling (#22)
    * The first pass of the human entry columns has been implemented. This allows us to generate the different options that can be used.
    * Fixed issues where the newly generated columns don't get added to the deltas
    * Added the human suggestions sheet to the source control to keep track of what change tree
    * Included a patch file for the scenario where another specification was the amount instead.
    * Added more code for preventing 'null'ing of unmodified fields
    * Included the latest generated patch file from the new workflow

---
+++ utils.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import traceback
+from pipeline.patch.patchfile import PatchFile, generate_update_patches
+import pandas as pd
+
+
+def generate_update_patch_file(
+    old_df: pd.DataFrame,
+    new_df: pd.DataFrame,
+    patch_comment: str,
+    trace: bool = False,
+    new_file_name: str = None,
+    outpath: Path = Path("./"),
+    id_columns: list = ["hhid", "redcap_event_name"],
+):
+    try:
+        patches = generate_update_patches(old_df, new_df, id_columns)
+    except Exception as e:
+        print(f"Error generating patches: {e}")
+        if trace:
+            traceback.print_exc()
+        exit(1)
+
+    patch_file = PatchFile(
+        creations=[],
+        deletions=[],
+        patches=patches,
+        version=1,
+        meta={
+            "notes": patch_comment,
+        },
+    )
+
+    # Create directories if they dont exist for outpath:
+    outpath.mkdir(parents=True, exist_ok=True)
+
+    out_file_path = outpath / f"{new_file_name}.json"
+
+    with open(out_file_path.absolute(), "w") as file_ptr:
+        file_ptr.write(patch_file.get_json())
