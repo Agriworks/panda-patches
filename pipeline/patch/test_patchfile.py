d3e8f4a788553ab79b0c786ee81bb884152e609f
Radhakrishna Sanka <rkrishnasanka@gmail.com>
2023-11-27 11:53:38 +0530

    Fixes/other entry handler (#28)
    * Saving all the other human entry stuff
    * Add support for different file extensions in the data
    loading and processing
    * Started creating a couple of tests for patches and other entry handlers
    * Add patch classes for creating, deleting, and
    updating rows in a DataFrame
    * Fix stripping of replacement values in
    other_entry_handler.py
    * Created a couple of tests and got the delete triggers to work correctly, also verified that replacement works okay
    TODO: Need to add more tests for replacement triggers
    * Added the human entry sheets and the corresponding not-null references. However, we have disabled the not null filtering for the time being.
    * Fixed DeepDiff dependency after the snafu
    * Updated the patch file now considering that all the other pieces are fixed, I need to check with Divya to make sure that the changes are correct.
    * Updated the other file since to normalize a couple of things

--- pipeline/patch/test_patchfile.py
+++ pipeline/patch/test_patchfile.py
@@ -2,11 +2,11 @@ import json
 from typing import Any
 from deepdiff import DeepDiff
 from tests.conftest import sample_patch_json, sample_data_frame
-from pipeline.patch.patchfile import  Patch, PatchFile,  generate_patches
+from pipeline.patch.patchfile import Patch, PatchFile, UpdatePatch, generate_patches
 
 json_dict = {}
 with open("patchfile_example.json", "r") as file_ptr:
-     json_dict = json.load(file_ptr)
+    json_dict = json.load(file_ptr)
 
 # patch_file = PatchFile.parse_patch_file_json_dict(json_dict)
 
@@ -21,41 +21,63 @@ data = {
     "status": ["old", "old", "old"],
 }
 patch_data = {
-     "target": {"id": 1, "id2": 4},
-     "deltas": {"value": 100, "status": "updated"},
- }
+    "target": {"id": 1, "id2": 4},
+    "deltas": {"value": 100, "status": "updated"},
+}
 target_data = {
-        "id": [1, 2, 3],
-        "id2": [3, 4, 5],
-        "value": [100, 60, 70],
-        "status": ["updated", "old", "old"]}
-class TestPatch:
-    def test_parse_patch_object_json_dict(sample_patch_json: dict[str, Any]):
-        json_dict = {}
-        with open("patchfile_example.json", "r") as file_ptr:
-            json_dict = json.load(file_ptr)
-
-        print(json_dict)
-
-        patch_file = PatchFile.parse_patch_file_json_dict(json_dict)
-
-        assert not DeepDiff(patch_file.__dict__, sample_patch_json, ignore_order=True)
-
-    def test_apply(sample_data_frame):
-        patch = Patch.parse_patch_object_json_dict(patch_data)
-        patch.apply(data)
-        assert data == target_data
+    "id": [1, 2, 3],
+    "id2": [3, 4, 5],
+    "value": [100, 60, 70],
+    "status": ["updated", "old", "old"],
+}
 
 
-  
+# class TestPatch:
+def test_parse_patch_object_json_dict(sample_patch_json: dict[str, Any]):
+    json_dict = {}
+    with open("patchfile_example.json", "r") as file_ptr:
+        json_dict = json.load(file_ptr)
+
+    print(json_dict)
+
+    patch_file = PatchFile.parse_patch_file_json_dict(json_dict)
+
+    assert not DeepDiff(patch_file.__dict__, sample_patch_json, ignore_order=True)
 
 
-class TestPatchFile:
-    def test_parse_patch_file_json_dict():
-        assert False
+def test_apply(sample_data_frame):
+    patch = Patch.parse_patch_object_json_dict(patch_data)
+    patch.apply(data)
+    assert data == target_data
 
-    def test_save_json():
-        assert False
+
+def test_eq():
+    assert False
+
+
+def test_parse_patch_object_json_dict():
+    assert False
+
+
+def test_apply():
+    assert False
+
+
+def test_update_patch_eq():
+    update1 = UpdatePatch(target={"id": 1, "id2": 4}, deltas={"value": 100})
+    update2 = UpdatePatch(target={"id": 1, "id2": 4}, deltas={"value": 100})
+    update3 = UpdatePatch(target={"id": 1, "id2": 4}, deltas={"value": 200})
+
+    assert update1 == update2
+    assert update1 != update3
+
+
+def test_parse_patch_file_json_dict():
+    assert False
+
+
+def test_save_json():
+    assert False
 
 
 """ TODO - Cannibalize this into the test code
